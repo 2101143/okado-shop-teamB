14fd3c7b573906ef1b57326f7c570ed9a9208746
masayoshi <2101138@s.asojuku.ac.jp>
2022-12-08 19:50:54 +0900

    Update okado_insert.sql

--- okado_insert.sql
+++ okado_insert.sql
@@ -9,99 +9,119 @@ INSERT INTO `workshop`( `workshop_name`, `workshop_date`, `workshop_start`, `wor
                     ("workshop1", '2022-12-01', '12:00', '13:00', "cafeteria1", 1000, 3, "ワークショップ１");
 
 INSERT INTO `item_categories`(`item_category_name`)
-            VALUES ("ネックレス"),
-                    (""),
-                    (""),
-                    ("");
+                    VALUES  ("ネックレス"),
+                            (""),
+                            (""),
+                            ("");
 
-VALUES ("", '', '', '', "", , , ""),
-        ("", '', '', '', "", , , ""),
-        ("", '', '', '', "", , , ""),
-        ("", '', '', '', "", , , "");
+INSERT INTO `items`(`item_category_id`, `item_name`, `item_price`, `item_description`, `item_inventory`, `item_pickupflag`)
+            VALUES  (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                *10 (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                *20 (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                *30 (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                    (, "", , "", , ),
+                *40 (, "", , "", , );
 
-CREATE TABLE workshop_reserve
-(workshop_reserve_id INT AUTO_INCREMENT,
-workshop_id INT NOT NULL,
-user_id INT NOT NULL,
-workshop_reserve_people INT NOT NULL,
-PRIMARY KEY(workshop_reserve_id),
-FOREIGN KEY fkey1(workshop_id)
-	REFERENCES workshop(workshop_id),
-FOREIGN KEY fkey2(user_id)
-    REFERENCES user(user_id)
-);
+INSERT INTO `item_images`(`item_id`, `item_image_order`, `item_image_path`)
+                VALUES  (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                    *10 (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                    *20 (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                    *30 (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                    *40 (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                    *50 (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                    *60 (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , ""),
+                        (, , "");
 
-CREATE TABLE item_categories
-(item_category_id INT AUTO_INCREMENT,
-item_category_name VARCHAR(20) NOT NULL,
-PRIMARY KEY(item_category_id)
-);
-
-CREATE TABLE items
-(item_id INT AUTO_INCREMENT,
-item_category_id INT NOT NULL,
-item_name VARCHAR(50) NOT NULL,
-item_price INT NOT NULL,
-item_description VARCHAR(800) NOT NULL,
-item_inventory INT NOT NULL,
-item_pickupflag BOOLEAN NOT NULL,
-PRIMARY KEY (item_id),
-FOREIGN KEY fkey1(item_category_id)
-	REFERENCES item_categories(item_category_id)
-);
-
-CREATE TABLE item_images
-(item_image_id INT AUTO_INCREMENT,
-item_id INT NOT NULL,
-item_image_order INT NOT NULL,
-item_image_path VARCHAR(191) NOT NULL,
-PRIMARY KEY(item_image_id),
-FOREIGN KEY fkey1(item_id)
-    REFERENCES items(item_id)
-);
-
-CREATE TABLE favorite_items
-(favorite_item_id INT AUTO_INCREMENT,
-item_id INT NOT NULL,
-user_id INT NOT NULL,
-PRIMARY KEY(favorite_item_id),
-FOREIGN KEY fkey1(item_id)
-	REFERENCES items(item_id),
-FOREIGN KEY fkey2(user_id)
-	REFERENCES user(user_id)
-);
-
-CREATE TABLE carts
-(cart_id INT AUTO_INCREMENT,
-user_id INT NOT NULL,
-item_id INT NOT NULL,
-cart_quantity INT NOT NULL,
-PRIMARY KEY(cart_id),
-FOREIGN KEY fkey1(user_id)
-	REFERENCES user(user_id),
-FOREIGN KEY fkey2(item_id)
-	REFERENCES items(item_id)
-);
-
-CREATE TABLE purchase_histories
-(purchase_id INT AUTO_INCREMENT,
-user_id INT NOT NULL,
-purchase_date DATE NOT NULL,
-purchase_amount INT NOT NULL,
-PRIMARY KEY(purchase_id),
-FOREIGN KEY fkey1(user_id)
-	REFERENCES user(user_id)
-);
-
-CREATE TABLE purchase_details
-(purchase_deatail_id INT AUTO_INCREMENT,
-purchase_id INT NOT NULL,
-item_id INT NOT NULL,
-item_price INT NOT NULL,
-purchase_detail_quantity INT NOT NULL,
-PRIMARY KEY(purchase_deatail_id),
-FOREIGN KEY fkey1(purchase_id)
-    REFERENCES purchase_histories(purchase_id),
-FOREIGN KEY fkey2(item_id)
-    REFERENCES items(item_id)
-);
