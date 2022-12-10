233642580b6824a3fae6f173d6095b90e5ba396a
masayoshi <2101138@s.asojuku.ac.jp>
2022-12-10 15:12:28 +0900

    test

--- home_test1.php
+++ home_test1.php
@@ -6,8 +6,19 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="./css/style_test.css">
+    <link rel="stylesheet"  type="text/css" href="./css/style_test.css">
 </head>
-    
 <body>
+    <header>
+        <ul>
+            <li>list1</li>
+            <li>list2</li>
+            <li>list3</li>
+            <li>list4</li>
+            <li>list5</li>
+        </ul>
+    </header>
+    <p></p>
+    <h1>はじめまして</h1>
+</body>
     
